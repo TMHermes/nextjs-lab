19a850eaca5a84e49d6e95258f2d103e26fac273
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-12-02 20:02:05 -0300

    myfirstReact

---
+++ hoc/comAutorizacao.js
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+
+export default function comAutorizacao(Componente) {
+    const estaLogado = true;
+    
+    return (props) => {
+        if(typeof window === 'undefined') {
+            return null;
+        }
+
+        const router = useRouter();
+        if(estaLogado) {
+            return <Componente {...props} />
+    }
+    
+    router.push('/login');
+}
+}
